37b1e44d9869a980f907f135928a834cc1ccfffd
Tarik Horozovic <tarik@finit.ba>
2023-06-09 10:25:15 +0200

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RotaryClub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
