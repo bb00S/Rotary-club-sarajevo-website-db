cedc51c0fa55888953ae428abc344377d048176c
Tarik Horozovic <tarik@finit.ba>
2023-06-13 13:36:26 +0200

    Add Member model and migration Remove unnececary SCSS

---
+++ Models/Member.cs
@@ -0,0 +1,14 @@
+﻿namespace RotaryClub.Models
+{
+    public class Member
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get;set; }
+        public string Website { get; set; }
+        public string PhotoUrl { get;set; }
+        public bool Honorary { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
