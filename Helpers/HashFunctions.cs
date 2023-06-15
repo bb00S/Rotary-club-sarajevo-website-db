40582b0165f867ef26be5feea51e1c35ad791a05
herr-hauptmann <tarik.horoz@gmail.com>
2023-06-15 09:19:31 +0200

    Create seeder migration, separate Hash functions to helper class

---
+++ Helpers/HashFunctions.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Cryptography;
+
+namespace RotaryClub.Helpers
+{
+    public static class HashFunctions
+    {
+        public static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        public static string CreateRandomToken()
+        {
+            return Convert.ToHexString(RandomNumberGenerator.GetBytes(64));
+        }
+
+        public static bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);
+            }
+        }
+    }
+}
